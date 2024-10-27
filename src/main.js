c1ecbf3c158cf8aefea8b79b1e0879b093f1032b
OmarReyesC <omareyescar@gmail.com>
2024-10-27 14:59:35 -0600

    Class 9 completed. Basic navigation

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ async function getTrendingMoviesPreview() {
   console.log({data, movies});
 
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
 
@@ -27,7 +27,7 @@ async function getTrendingMoviesPreview() {
     );
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
 
   });
 };
@@ -38,7 +38,7 @@ async function getCategoriesPreview() {
   console.log({data, categories});
 
   categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
 
@@ -49,7 +49,7 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer);
+    categoriesPreviewList.appendChild(categoryContainer);
 
   });
 };
